2a25ac0e12f576414866ad702149902309b7d74e
ashish-witarist <ashish@witarist.com>
2025-09-02 14:13:33 +0530

    tnmc changed

--- src/pages/fertility-experts/[slug].tsx
+++ src/pages/fertility-experts/[slug].tsx
@@ -208,7 +208,8 @@ const Doctor = ({ doctor }) => {
                   <div className='mb-2 text-gray-800 dark:text-gray-200'>{doctor.designation}</div>
                   {doctor?.medicalRegNo && (
                     <div className='mb-2 text-gray-800 dark:text-gray-200'>
-                      மருத்துவ பதிவு எண். (TNMC) :{' '}
+                      மருத்துவ பதிவு எண்.{' '}
+                      {doctor?.slug === 'dr-radha-puchalapalli' ? '(TNMC)' : '(KMC)'} :{' '}
                       <span className='font-bold underline'>{doctor?.medicalRegNo}</span>
                     </div>
                   )}
